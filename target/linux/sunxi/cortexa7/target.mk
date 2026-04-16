b2300ecde2a0fdf5002ca7524a4f96935aa46aa4
brudalevante <170347513+brudalevante@users.noreply.github.com>
2026-04-16 06:23:25 +0000

    primer commit limpio

---
+++ target/linux/sunxi/cortexa7/target.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2017 Hauke Mehrtens
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Allwinner A20/A3x/H3/R40
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
+FEATURES+=fpu
+
+DEFAULT_PACKAGES += wpad-basic-mbedtls
